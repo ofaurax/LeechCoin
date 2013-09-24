9d4ef6a1cca65f678984860245083d03ae191fab
Olivier FAURAX <olivier+git@faurax.fr>
2013-09-24 14:13:30 +0200

    Alpha1 * option -d pour spécifier la DB * ajout commande : list * ne retélécharge pas les annonces déjà connues * gestion des exceptions * passage des mois en numérique et ajout de l'année * format de DB qui inclut l'année

--- leechcoin.py
+++ leechcoin.py
@@ -5,19 +5,35 @@ import argparse
 import urllib2  # python 2.7
 import re
 import sqlite3
+import traceback
+import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('cmd')
+parser.add_argument('-d',
+                    help='database name to use (default:database.db)',
+                    default='database.db')
 parser.add_argument('params', nargs='*')
 
 args = parser.parse_args()
 
 print 'Commande :', args.cmd
-print 'Args :', args.params
+print 'Args :', args
 cmd = args.cmd
+database = args.d
+
+fgen = u'{0} {1:6}€ {2:3}m² {3} {4:30} {5:25} {6}/{7}/{8} {9}'
+fdb = u'DB : ' + fgen
 
 if cmd == 'help':
     print 'leech [num]: download of data from page [num] (default:1)'
+
+if cmd == 'list':
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+    
+    for tmp in c.execute('SELECT * FROM apparts'):
+        print fdb.format(*tmp)
 
 if cmd == 'leech':
 
@@ -26,21 +42,29 @@ if cmd == 'leech':
     except:
         page = 1
 
-    conn = sqlite3.connect('leechcoin.db')
+    conn = sqlite3.connect(database)
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS apparts (id text, prix int, surface int, cp int, ville text, nom text, jour int, mois text, heure text)')
+    c.execute('CREATE TABLE IF NOT EXISTS apparts (id text PRIMARY KEY, prix int, surface int, cp int, ville text, nom text, jour int, mois int, annee int, heure text)')
 
     response = urllib2.urlopen(
         'http://www.leboncoin.fr/ventes_immobilieres/offres/'
         + 'provence_alpes_cote_d_azur/bouches_du_rhone/'
         + '?pe=8&sqs=6&ros=2&ret=1&ret=2&f=p&o=' + str(page))
     
-    re_ids = re.compile('ventes_immobilieres/([0-9]+)\.htm')
+    re_ids = re.compile('ventes_immobilieres/(?P<id>[0-9]+)\.htm')
     m = re_ids.finditer(response.read())
 
     for m2 in m:
-        id = m2.group(1)
+
+        id = m2.group("id")
         url = 'http://www.leboncoin.fr/ventes_immobilieres/' + id + '.htm'
+
+        c.execute('SELECT * FROM apparts WHERE id=?', (id,))
+        tmp = c.fetchone()
+        if(tmp):
+            print fdb.format(*tmp)
+            continue
+
         try:
             response = urllib2.urlopen(url)
         except Exception as e:
@@ -49,36 +73,64 @@ if cmd == 'leech':
             continue
         rep = response.read()
 
-        m3 = re.findall('class="price"\>([0-9 ]+).*\<', rep)
-        prix = int(m3[0].replace(' ', ''))
-        m3 = re.findall(
-            '<th>Surface : </th>\s*<td>([0-9]+) m<sup>2</sup>', rep)
-        surface = int(m3[0])
-        m3 = re.findall(
-            '<th>Code postal :</th>\s*<td>([0-9]+)</td>', rep)
-        cp = m3[0]
-        m3 = re.findall(
-            '<th>Ville :</th>\s*<td>([^<]+)</td>', rep)
-        ville = m3[0].decode('cp1252')
-        m3 = re.findall(
-            'Mise en ligne par <a rel="nofollow" '
-            + 'href="http://www2.leboncoin.fr/ar.ca=21_s&amp;id=[0-9]+" '
-            + 'onclick="return [^"]+">([^<]+)</a> '
-            + 'le (\d+) (.+) &agrave; (\d+:\d+). </div>', rep)
+        try:
+            m3 = re.findall('class="price"\>([0-9 ]+).*\<', rep)
+            prix = int(m3[0].replace(' ', ''))
+            m3 = re.findall(
+                '<th>Surface : </th>\s*<td>([0-9]+) m<sup>2</sup>', rep)
+            surface = int(m3[0])
+            m3 = re.findall(
+                '<th>Code postal :</th>\s*<td>([0-9]+)</td>', rep)
+            cp = m3[0]
+            m3 = re.findall(
+                '<th>Ville :</th>\s*<td>([^<]+)</td>', rep)
+            ville = m3[0].decode('cp1252')
+            m3 = re.findall(
+                'Mise en ligne par <a rel="nofollow" '
+                + 'href="http://www2.leboncoin.fr/ar.ca=21_s&amp;id=[0-9]+" '
+                + 'onclick="return [^"]+">([^<]+)</a> '
+                + 'le (\d+) (.+) &agrave; (\d+:\d+). </div>', rep)
         #print m3, m3[0]
-        nom = m3[0][0].decode('cp1252')
-        jour = m3[0][1]
-        mois = m3[0][2].decode('cp1252')
-        heure = m3[0][3]
+            nom = m3[0][0].decode('cp1252')
+            jour = m3[0][1]
+            mois = m3[0][2].decode('cp1252')
+            if mois[:4] == 'janv' : mois = 1
+            elif mois[0] == 'f' : mois = 2
+            elif mois[:4] == 'mars' : mois = 3
+            elif mois[:4] == 'avri' : mois = 4
+            elif mois[:3] == 'mai' : mois = 5
+            elif mois[:4] == 'juin' : mois = 6
+            elif mois[:4] == 'juil' : mois = 7
+            elif mois[0] == 'a' : mois = 8
+            elif mois[:4] == 'sept' : mois = 9
+            elif mois[:4] == 'octo' : mois = 10
+            elif mois[:3] == 'nov' : mois = 11
+            elif mois[:3] == 'déc' : mois = 12
+            else : mois = 0
+            
+            ddt = datetime.datetime.today()
+            annee = ddt.year
+            # si on est en début d'année (ex: 3 jan 2014)
+            # et que l'annonce est d'un mois de fin d'année (ex: 20 déc)
+            # on corrige l'année (ex: pour avoir 20 déc 2013)
+            if ddt.month < 6 and mois > 6:
+                annee -= 1
+
+            heure = m3[0][3]
+        except Exception as e:
+            print 'Error on url', url
+            traceback.print_exc()
+            #print e
+            continue
 
         #print ville
-        f = u'{0:6}€ {1:3}m² {4:4}€/m² {2:5} {3:22} {6:20} {7}{8}@{9} {5}'
+        f = u'{0:6}€ {1:3}m² {4:4}€/m² {2:5} {3:22} {6:20} {7}/{8}/{9}@{10} {5}'
         print f.format(
             prix, surface, cp, ville, prix / surface,
-            url, nom, jour, mois[:4], heure)
+            url, nom, jour, mois, annee, heure)
 
-        c.execute('INSERT INTO apparts VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', \
-                  (id, prix, surface, cp, ville, nom, jour, mois, heure))
+        c.execute('INSERT INTO apparts VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', \
+                  (id, prix, surface, cp, ville, nom, jour, mois, annee, heure))
 
         #exit(0)
 
